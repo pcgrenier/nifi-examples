00ab44725bfedf47786a59bda597b439b7d8ba91
pcgrenier <pcgrenier@gmail.com>
2015-02-05 21:57:57 -0500

    Init

---
+++ src/main/java/rocks/nifi/examples/processors/JsonProcessor.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rocks.nifi.examples.processors;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.AbstractProcessor;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.ProcessorInitializationContext;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+
+/**
+ *
+ * @author phillip
+ */
+public class JsonProcessor extends AbstractProcessor {
+    
+    private List<PropertyDescriptor> properties;
+    private Set<Relationship> relationships;
+    
+    public static final PropertyDescriptor JSON_PATH = new PropertyDescriptor.Builder()
+            .name("Json Path")
+            .required(true)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+    
+    public static final Relationship SUCCESS = new Relationship.Builder()
+            .name("SUCCESS")
+            .description("Succes relationship")
+            .build();
+    
+    @Override
+    public void init(final ProcessorInitializationContext context){
+        List<PropertyDescriptor> properties = new ArrayList<>();
+        properties.add(JSON_PATH);
+        this.properties = Collections.unmodifiableList(properties);
+        
+        Set<Relationship> relationships = new HashSet<>();
+        relationships.add(SUCCESS);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+    
+    @Override
+    public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
+        FlowFile flowfile = session.get();
+        
+        
+    }
+    
+}
